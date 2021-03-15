c03ceb378ff961b5a7b8f79e3d81f5d995bc837c
theoistic <theo@theoistic.com>
2021-03-15 17:11:18 +0000

    minor mem calc change

--- rootfs/root/.config/i3/block_status.sh
+++ rootfs/root/.config/i3/block_status.sh
@@ -2,7 +2,7 @@
 
 while :
 do
-    memory_usage=`free -m | awk 'NR==2{print ($4/$3)*100 }'`
+    memory_usage=`free -m | awk 'NR==2{print (($3+$5)*100)/$2 }'`
     echo "MEM: ${memory_usage%.*}% | $(date) "
     sleep 5
 done
